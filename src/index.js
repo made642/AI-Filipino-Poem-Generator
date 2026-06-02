0791047ab15e72afe22dbc5a21ed06ab8e8d84ad
made642 <mcanada642@gmail.com>
2026-06-02 13:49:40 +0800

    updated the JS and prompt

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
+  console.log("Generated poem:", response.data);
   new Typewriter("#poemOutput", {
-    strings: [
-      "I have a Filipino dream!",
-      "A dream of a nation where every child can reach for the stars.",
-      "A dream of a country where poverty is just a memory.",
-      "I have a Filipino dream!",
-    ],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
+    deleteSpeed: null,
   });
 }
-let poemFormElement = document.querySelector("button");
-poemFormElement.addEventListener("click", generatePoem);
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#themeInput");
+  let apiKey = "ao8ce12baa0at03effcb000a09841c63";
+  let prompt = `provide a short 4 line filipino poem about ${instructionsInput.value} and make sure to use simple language that is easy to understand with <br> line breaks between each line in html format and NOT include the "html" tags. Then add strong "Shecode AI" tags after the last line.Make sure that each line is separated by a line break in html format.`;
+  let context =
+    "provide a clear and precise answer with <br> line breaks between each line in html format and NOT include the 'html' tags. Then add strong 'Shecode AI' tags after the last line.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poemOutput");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a Filipino poem about ${instructionsInput.value}</div>`;
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+  axios.get(apiUrl).then(displayPoem);
+}
+
+let poemFormElement = document.querySelector("#poemForm");
+poemFormElement.addEventListener("submit", generatePoem);
